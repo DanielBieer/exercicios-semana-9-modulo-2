f35e387d613ad10068aeb06df2745259a7f9cbc5
DanielBieer <danibierdasilva@gmail.com>
2024-08-18 23:36:08 -0300

    teste

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 
-import React from 'react';
-import { useAuth } from '../contexts/AuthContext/AuthContext';
+
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext/AuthContext';
 
 const Dashboard = () => {
   const { user, signOut } = useAuth();
